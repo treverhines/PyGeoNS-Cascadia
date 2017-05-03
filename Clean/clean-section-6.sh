af70f8bf9bd8bd49563d5f7f0578369a40f82699
hinest <treverhines@gmail.com>
2017-05-03 15:13:12 -0400

    added more

--- Clean/clean-section-6.sh
+++ Clean/clean-section-6.sh
@@ -20,15 +20,15 @@ MAX_LON="-121.0"
 MIN_LAT="40.0"
 MAX_LAT="42.0"
 
-mkdir -p work/section-3/2017-2018
-mkdir -p work/section-3/2016-2017
-mkdir -p work/section-3/2015-2016
-mkdir -p work/section-3/2014-2015
-mkdir -p work/section-3/2013-2014
-mkdir -p work/section-3/2012-2013
-mkdir -p work/section-3/2011-2012
-mkdir -p work/section-3/2010-2011
-mkdir -p work/section-3/2009-2010
+mkdir -p work/section-6/2017-2018
+mkdir -p work/section-6/2016-2017
+mkdir -p work/section-6/2015-2016
+mkdir -p work/section-6/2014-2015
+mkdir -p work/section-6/2013-2014
+mkdir -p work/section-6/2012-2013
+mkdir -p work/section-6/2011-2012
+mkdir -p work/section-6/2010-2011
+mkdir -p work/section-6/2009-2010
 
 #for yr in 2009 2010 2011 2012 2013 2014 2015 2016 2017
 for yr in 2009 2017
@@ -41,21 +41,21 @@ for yr in 2009 2017
           --max-lon $MAX_LON \
           --start-date $yr-01-01 \
           --stop-date $yrp1-01-01 \
-          --output-stem work/section-3/$yr-$yrp1/data \
-          -vv &>> work/section-3.log
-  pygeons autoclean work/section-3/$yr-$yrp1/data.h5 \
-          --output-stem work/section-3/$yr-$yrp1/data.clean \
-          -vv &>> work/section-3.log
+          --output-stem work/section-6/$yr-$yrp1/data \
+          -vv &>> work/section-6.log
+  pygeons autoclean work/section-6/$yr-$yrp1/data.h5 \
+          --output-stem work/section-6/$yr-$yrp1/data.clean \
+          -vv &>> work/section-6.log
   done
 
-pygeons merge work/section-3/2009-2010/data.clean.h5 \
-              work/section-3/2010-2011/data.clean.h5 \
-              work/section-3/2011-2012/data.clean.h5 \
-              work/section-3/2012-2013/data.clean.h5 \
-              work/section-3/2013-2014/data.clean.h5 \
-              work/section-3/2014-2015/data.clean.h5 \
-              work/section-3/2015-2016/data.clean.h5 \
-              work/section-3/2016-2017/data.clean.h5 \
-              work/section-3/2017-2018/data.clean.h5 \
-        --output-stem work/section-3/data.clean.merged \
-        -vv &>> work/section-3.log
+pygeons merge work/section-6/2009-2010/data.clean.h5 \
+              work/section-6/2010-2011/data.clean.h5 \
+              work/section-6/2011-2012/data.clean.h5 \
+              work/section-6/2012-2013/data.clean.h5 \
+              work/section-6/2013-2014/data.clean.h5 \
+              work/section-6/2014-2015/data.clean.h5 \
+              work/section-6/2015-2016/data.clean.h5 \
+              work/section-6/2016-2017/data.clean.h5 \
+              work/section-6/2017-2018/data.clean.h5 \
+        --output-stem work/section-6/data.clean.merged \
+        -vv &>> work/section-6.log
--- SSEModel-MAT32/make-sse-files.sh
+++ SSEModel-MAT32/make-sse-files.sh
@@ -4,133 +4,158 @@ mkdir -p sse
 
 # Catalog of large SSEs
  
+# 2009a
+# lat : 43.5 - 47.5
+# date : 2009-07-01 - 2009-11-01
+mkdir -p sse/2009a
+pygeons crop data.final.h5 \
+        --start-date 2009-07-01 \
+        --stop-date 2009-11-01 \
+        --min-lat 43.5 \
+        --max-lat 47.5 \
+        --max-lon -120.0 \
+        --output-stem sse/2009a/data \
+        -vv
+
 # 2010a
-# lat : 45.0 - 50.0
-# date : 2010-07-01 - 2010-10-01
+# lat : 45.5 - 51.0
+# date : 2010-07-01 - 2010-11-01
 mkdir -p sse/2010a
 pygeons crop data.final.h5 \
         --start-date 2010-07-01 \
-        --stop-date 2010-10-01 \
-        --min-lat 45.0 \
-        --max-lat 50.0 \
+        --stop-date 2010-11-01 \
+        --min-lat 45.5 \
+        --max-lat 51.0 \
         --max-lon -120.0 \
         --output-stem sse/2010a/data \
         -vv
 
 # 2011a
-# lat : 43.5 - 47.0
-# date : 2011-05-01 - 2011-08-01
+# lat : 43.5 - 47.5
+# date : 2011-05-01 - 2011-09-01
 mkdir -p sse/2011a
 pygeons crop data.final.h5 \
         --start-date 2011-05-01 \
-        --stop-date 2011-08-01 \
+        --stop-date 2011-09-01 \
         --min-lat 43.5 \
-        --max-lat 47.0 \
+        --max-lat 47.5 \
         --max-lon -120.0 \
         --output-stem sse/2011a/data \
         -vv
 
 # 2011b
-# lat : 45.5 - 50.0
-# date : 2011-07-01 - 2011-10-01
+# lat : 45.5 - 51.0
+# date : 2011-07-01 - 2011-11-01
 mkdir -p sse/2011b
 pygeons crop data.final.h5 \
         --start-date 2011-07-01 \
-        --stop-date 2011-10-01 \
+        --stop-date 2011-11-01 \
         --min-lat 45.5 \
-        --max-lat 50.0 \
+        --max-lat 51.0 \
         --max-lon -120.0 \
         --output-stem sse/2011b/data \
         -vv
 
 # 2012a
-# lat : 46.0 - 51.0
-# date : 2012-08-01 - 2012-11-01
+# lat : 45.5 - 51.0
+# date : 2012-08-01 - 2012-12-01
 mkdir -p sse/2012a
 pygeons crop data.final.h5 \
         --start-date 2012-08-01 \
-        --stop-date 2012-11-01 \
-        --min-lat 46.0 \
+        --stop-date 2012-12-01 \
+        --min-lat 45.5 \
         --max-lat 51.0 \
         --max-lon -120.0 \
         --output-stem sse/2012a/data \
         -vv
 
 # 2013a
-# lat : 43.5 - 48.0
+# lat : 43.5 - 47.5
 # date : 2013-02-01 - 2013-06-01
 mkdir -p sse/2013a
 pygeons crop data.final.h5 \
         --start-date 2013-02-01 \
         --stop-date 2013-06-01 \
         --min-lat 43.5 \
-        --max-lat 48.0 \
+        --max-lat 47.5 \
         --max-lon -120.0 \
         --output-stem sse/2013a/data \
         -vv
 
 # 2013b
-# lat : 46.5 - 51.0
+# lat : 45.5 - 51.0
 # date : 2013-07-01 - 2013-11-01
 mkdir -p sse/2013b
 pygeons crop data.final.h5 \
         --start-date 2013-07-01 \
         --stop-date 2013-11-01 \
-        --min-lat 46.5 \
+        --min-lat 45.5 \
         --max-lat 51.0 \
         --max-lon -120.0 \
         --output-stem sse/2013b/data \
         -vv
 
 # 2014a
-# lat : 46.5 - 50.0 
-# date : 2014-10-01 - 2015-01-01
+# lat : 45.5 - 51.0
+# date : 2014-10-01 - 2015-02-01
 mkdir -p sse/2014a
 pygeons crop data.final.h5 \
         --start-date 2014-10-01 \
-        --stop-date 2015-01-01 \
-        --min-lat 46.5 \
-        --max-lat 50.0 \
+        --stop-date 2015-02-01 \
+        --min-lat 45.5 \
+        --max-lat 51.0 \
         --max-lon -120.0 \
         --output-stem sse/2014a/data \
         -vv
 
 # 2015a
-# lat : 46.0 - 51.0
+# lat : 45.5 - 51.0
 # date : 2015-11-01 - 2016-03-01
 mkdir -p sse/2015a
 pygeons crop data.final.h5 \
         --start-date 2015-11-01 \
         --stop-date 2016-03-01 \
-        --min-lat 46.0 \
+        --min-lat 45.5 \
         --max-lat 51.0 \
         --max-lon -120.0 \
         --output-stem sse/2015a/data \
         -vv
 
 # 2016a
-# lat : 43.0 - 47.0
+# lat : 43.5 - 47.5
 # date : 2015-12-01 - 2016-04-01
 mkdir -p sse/2016a
 pygeons crop data.final.h5 \
         --start-date 2015-12-01 \
         --stop-date 2016-04-01 \
-        --min-lat 43.0 \
-        --max-lat 47.0 \
+        --min-lat 43.5 \
+        --max-lat 47.5 \
         --max-lon -120.0 \
         --output-stem sse/2016a/data \
         -vv
 
+# 2017a
+# lat : 45.5 - 51.0
+# date : 2017-01-01 - 2017-05-01
+mkdir -p sse/2017a
+pygeons crop data.final.h5 \
+        --start-date 2017-01-01 \
+        --stop-date 2017-05-01 \
+        --min-lat 45.5 \
+        --max-lat 51.0 \
+        --max-lon -120.0 \
+        --output-stem sse/2017a/data \
+        -vv
 
 # null (no signal present)
-# lat : 44.0 - 48.0 
+# lat : 43.5 - 47.5
 # date : 2015-01-01 - 2015-05-01
 mkdir -p sse/null
 pygeons crop data.final.h5 \
         --start-date 2015-01-01 \
         --stop-date 2015-05-01 \
-        --min-lat 44.0 \
-        --max-lat 48.0 \
+        --min-lat 43.5 \
+        --max-lat 47.5 \
         --max-lon -120.0 \
         --output-stem sse/null/data \
         -vv
