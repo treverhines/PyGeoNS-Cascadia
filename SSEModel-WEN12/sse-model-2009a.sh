ef6f9e5467056cb7ffc242be6088b44d80d0ee35
hinest <treverhines@gmail.com>
2017-05-12 15:06:40 -0400

    about to make big changes to wen directory

---
+++ SSEModel-WEN12/sse-model-2009a.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#PBS -A ehetland_flux
+#PBS -M hinest@umich.edu
+#PBS -N sse-model-2009a
+#PBS -m abe
+#PBS -V
+#PBS -j oe
+#PBS -o sse-model-2009a.log
+#PBS -q flux
+#PBS -l qos=flux
+#PBS -l nodes=1:ppn=8,mem=32000mb,walltime=40:00:00
+cd $PBS_O_WORKDIR
+
+pygeons reml sse/2009a/data.h5 \
+          --network-model wen12-se \
+          --network-params 1.0 0.05 50.0 \
+          --station-model p0 p1 \
+          --station-params \
+          --station-fix \
+          -vv &> /scratch/ehetland_flux/hinest/2009a-wen.log
+
